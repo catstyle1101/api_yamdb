dabb1564db9187cc35243b1631fa75a4400823da
catstyle1101 <anton.m.rus@gmail.com>
2023-02-14 13:17:50 +0500

    branch fix

---
+++ api_yamdb/api/models/__init__.py
@@ -0,0 +1,5 @@
+from api.models.categories import Categories
+from api.models.comments import Comments
+from api.models.genres import Genre, GenreCategories
+from api.models.review import Review
+from api.models.titles import Titles
