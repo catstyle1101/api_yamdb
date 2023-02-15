b455ebf559c112a74cd1441cba74e7acd86c68b5
Антон Масюк <37059480+catstyle1101@users.noreply.github.com>
2023-02-15 15:30:53 +0500

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -12,7 +12,7 @@ router_v1 = routers.DefaultRouter()
 router_v1.register(
     r"titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments",
     CommentViewSet,
-    basename='comments'
+    basename='comments',
 )
 router_v1.register(
     r"titles/(?P<title_id>\d+)/reviews",
